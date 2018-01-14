82cfd35c12f0cc4122b58167f4de9e84989d0a11
Javier <javitoom96@gmail.com>
2018-01-14 20:34:16 +0100

    add: añade script de despliegue de subsistemas wordpress en servidor
    Añade el script para realizar el despliegue continuo de la rama master en los diferentes subsitemas (plugins) de WordPress

---
+++ tools/deploy-wordpress-subsistemas.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+function deploy_programa() {
+    cd ~/g2/wordpress/repo-plugins/"$1"
+    git pull
+    rm -r ~/g2/wordpress/splc/wp-content/plugins/"$1"
+    cp -r ~/g2/wordpress/repo-plugins/"$1" ~/g2/wordpress/splc/wp-content/plugins/
+}
+
+
+function deploy_redes() {
+  cd ~/g2/wordpress/repo-plugins/"$1"
+  git pull
+  rm -r ~/g2/wordpress/splc/wp-content/plugins/socialhub-egc
+  cp -r socialhub-egc ~/g2/wordpress/splc/wp-content/plugins/
+}
+
+
+function deploy_registro() {
+  cd ~/g2/wordpress/repo-plugins/"$1"
+  git pull
+  rm -r ~/g2/wordpress/splc/wp-content/plugins/Trabajo
+  cp -r Trabajo ~/g2/wordpress/splc/wp-content/plugins/
+}
+
+
+function main() {
+    case "$1" in
+    "project-program")
+        deploy_autenticacion "$@";;
+    "RedesSociales")
+        deploy_censos "$@";;
+    "Registro")
+        deploy_votaciones "$@";;
+    esac
+}
+
+set -e
+main "$@"
